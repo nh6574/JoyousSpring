622215b430d630fd8bc8192e4cb555ddf0f5fc72
Sebastián Venturino <venturinosebastian@gmail.com>
2025-04-03 17:43:19 -0300

    shaddoll rarities and costs

--- src/jokers/18Shaddoll.lua
+++ src/jokers/18Shaddoll.lua
@@ -30,7 +30,7 @@ SMODS.Joker({
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 3,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.h_size, card.ability.extra.revives, card.ability.extra.mills, card.ability.extra.turns } }
     end,
@@ -111,7 +111,7 @@ SMODS.Joker({
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 3,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.hands, card.ability.extra.revives, card.ability.extra.mills, card.ability.extra.turns } }
     end,
@@ -194,7 +194,7 @@ SMODS.Joker({
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 3,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.revives_flip, card.ability.extra.revives, card.ability.extra.mills, card.ability.extra.creates } }
     end,
@@ -261,7 +261,7 @@ SMODS.Joker({
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 3,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.creates_tarot, card.ability.extra.revives, card.ability.extra.mills, card.ability.extra.creates_spectral } }
     end,
@@ -327,7 +327,7 @@ SMODS.Joker({
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 6,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.money, card.ability.extra.revives, card.ability.extra.mills } }
     end,
@@ -382,7 +382,7 @@ SMODS.Joker({
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 3,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.discards, card.ability.extra.revives, card.ability.extra.mills, card.ability.extra.turns } }
     end,
@@ -465,7 +465,7 @@ SMODS.Joker({
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 5,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.xmult, card.ability.extra.revives, card.ability.extra.mills, card.ability.extra.creates } }
     end,
@@ -520,11 +520,11 @@ SMODS.Joker({
     key = "shaddoll_ariel",
     atlas = 'shaddoll',
     pos = { x = 1, y = 0 },
-    rarity = 1,
+    rarity = 2,
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 5,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.banishes } }
     end,
@@ -576,11 +576,11 @@ SMODS.Joker({
     key = "shaddoll_genius",
     atlas = 'shaddoll',
     pos = { x = 2, y = 0 },
-    rarity = 1,
+    rarity = 2,
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 5,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.flips } }
     end,
@@ -647,11 +647,11 @@ SMODS.Joker({
     key = "shaddoll_keios",
     atlas = 'shaddoll',
     pos = { x = 3, y = 0 },
-    rarity = 1,
+    rarity = 2,
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 5,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.adds, card.ability.extra.mult } }
     end,
@@ -715,11 +715,11 @@ SMODS.Joker({
     key = "shaddoll_wendi",
     atlas = 'shaddoll',
     pos = { x = 4, y = 0 },
-    rarity = 1,
+    rarity = 2,
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 5,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.creates, card.ability.extra.revives } }
     end,
@@ -779,11 +779,11 @@ SMODS.Joker({
     key = "shaddoll_hollow",
     atlas = 'shaddoll',
     pos = { x = 0, y = 0 },
-    rarity = 1,
+    rarity = 2,
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 5,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.mills, card.ability.extra.mills_transfer } }
     end,
@@ -845,11 +845,11 @@ SMODS.Joker({
     key = "shaddoll_anoya",
     atlas = 'shaddoll',
     pos = { x = 3, y = 2 },
-    rarity = 1,
+    rarity = 3,
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 10,
     loc_vars = function(self, info_queue, card)
         return {
             vars = { card.ability.extra.chips, card.ability.extra.chips *
@@ -931,11 +931,11 @@ SMODS.Joker({
     key = "shaddoll_apka",
     atlas = 'shaddoll',
     pos = { x = 4, y = 2 },
-    rarity = 1,
+    rarity = 2,
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 8,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.adds } }
     end,
@@ -1032,11 +1032,11 @@ SMODS.Joker({
     key = "shaddoll_winda",
     atlas = 'shaddoll',
     pos = { x = 4, y = 3 },
-    rarity = 1,
+    rarity = 3,
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 12,
     loc_vars = function(self, info_queue, card)
         local empty_spaces = G.jokers and math.max(0, G.jokers.config.card_limit - #G.jokers.cards + G.GAME.joker_buffer) or
             0
@@ -1127,11 +1127,11 @@ SMODS.Joker({
     key = "shaddoll_elconstruct",
     atlas = 'shaddoll',
     pos = { x = 0, y = 3 },
-    rarity = 1,
+    rarity = 2,
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 8,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.mills, card.ability.extra.adds } }
     end,
@@ -1208,11 +1208,11 @@ SMODS.Joker({
     key = "shaddoll_grysta",
     atlas = 'shaddoll',
     pos = { x = 1, y = 3 },
-    rarity = 1,
+    rarity = 3,
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 12,
     loc_vars = function(self, info_queue, card)
         return {
             vars = { tostring(card.ability.extra.xchips), 1 + card.ability.extra.xchips *
@@ -1294,11 +1294,11 @@ SMODS.Joker({
     key = "shaddoll_shekh",
     atlas = 'shaddoll',
     pos = { x = 2, y = 3 },
-    rarity = 1,
+    rarity = 3,
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 15,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.adds } }
     end,
@@ -1356,11 +1356,11 @@ SMODS.Joker({
     key = "shaddoll_wendigo",
     atlas = 'shaddoll',
     pos = { x = 3, y = 3 },
-    rarity = 1,
+    rarity = 2,
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 8,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.adds } }
     end,
@@ -1420,7 +1420,7 @@ SMODS.Joker({
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 6,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.adds } }
     end,
@@ -1492,7 +1492,7 @@ SMODS.Joker({
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
-    cost = 0,
+    cost = 7,
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.add_counter, card.ability.extra.counters, card.ability.extra.chips, card.ability.extra.chips * card.ability.extra.counters, card.ability.extra.remove, card.ability.extra.revives } }
     end,
