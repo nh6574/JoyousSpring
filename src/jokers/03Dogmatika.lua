613db6662db45d2472febe8fc3dfd47ac3ffb52a
Sebastián Venturino <venturinosebastian@gmail.com>
2025-10-29 18:14:01 -0300

    thundebolt info queue

--- src/jokers/03Dogmatika.lua
+++ src/jokers/03Dogmatika.lua
@@ -179,6 +179,9 @@ SMODS.Joker({
     eternal_compat = true,
     cost = 8,
     loc_vars = function(self, info_queue, card)
+        if not JoyousSpring.config.disable_tooltips and not card.fake_card and not card.debuff then
+            info_queue[#info_queue + 1] = { set = "Other", key = "joy_tooltip_extra_deck_joker" }
+        end
         return { vars = { card.ability.extra.xmult } }
     end,
     joy_desc_cards = {
