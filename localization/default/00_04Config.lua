0160029a1025daf630ced24b8baf9c1b695a8eb5
Sebastián Venturino <venturinosebastian@gmail.com>
2026-02-11 00:26:29 -0300

    change config wording

--- localization/default/00_04Config.lua
+++ localization/default/00_04Config.lua
@@ -7,11 +7,11 @@ return {
             k_joy_disable_tooltips = "Disable glossary tooltips",
             k_joy_disable_booster_tag = "Disable Booster Tag from appearing naturally",
             k_joy_disable_main_menu = "Disable Main Menu modifications",
-            k_joy_only_ygo_cards = "Only Monster Cards appear",
+            k_joy_only_ygo_cards = "Only Monster Cards appear (Recommended)",
             k_joy_extra_ygo_booster = "Add a Selection Pack to each shop (test feature)",
             k_joy_instant_poker_hand = "Disable poker hand level up animation (for this mod)",
             k_joy_disable_glow = "Disable glow effect on cards that can be activated",
-            k_joy_disable_side_deck = "Disable Side Deck"
+            k_joy_disable_side_deck = "Disable Side Deck (test feature)"
         }
     }
 }
