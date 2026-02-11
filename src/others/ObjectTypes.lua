521e1eb73b394fb2c8a583b24afe7a8b3befe4ed
Sebastián Venturino <venturinosebastian@gmail.com>
2026-02-10 21:23:21 -0300

    fix copies in pools

--- src/others/ObjectTypes.lua
+++ src/others/ObjectTypes.lua
@@ -21,26 +21,29 @@ SMODS.ObjectType({
 local SMODS_injectItems_ref = SMODS.injectItems
 function SMODS.injectItems()
     SMODS_injectItems_ref()
-    for i, v in ipairs(G.P_CENTER_POOLS.Joker) do
-        local monster_card_properties = v.config and v.config.extra and
-            type(v.config.extra) == "table" and
-            v.config.extra.joyous_spring
-        if monster_card_properties then
-            JoyousSpring.card_order[v.key] = i
-        end
-        if monster_card_properties and v.original_key ~= "token" then
-            SMODS.ObjectTypes["joy_monster"]:inject_card(v)
+    if not JoyousSpring.made_objectpool then
+        for i, v in ipairs(G.P_CENTER_POOLS.Joker) do
+            local monster_card_properties = v.config and v.config.extra and
+                type(v.config.extra) == "table" and
+                v.config.extra.joyous_spring
+            if monster_card_properties then
+                JoyousSpring.card_order[v.key] = i
+            end
+            if monster_card_properties and v.original_key ~= "token" then
+                SMODS.ObjectTypes["joy_monster"]:inject_card(v)
 
-            if not monster_card_properties.is_main_deck then
-                if monster_card_properties.is_field_spell then
-                    SMODS.ObjectTypes["joy_field"]:inject_card(v)
-                else
+                if not monster_card_properties.is_main_deck then
+                    if monster_card_properties.is_field_spell then
+                        SMODS.ObjectTypes["joy_field"]:inject_card(v)
+                    else
+                        SMODS.ObjectTypes["joy_extra"]:inject_card(v)
+                    end
+                end
+                if monster_card_properties and monster_card_properties.summon_type == "RITUAL" then
                     SMODS.ObjectTypes["joy_extra"]:inject_card(v)
                 end
             end
-            if monster_card_properties and monster_card_properties.summon_type == "RITUAL" then
-                SMODS.ObjectTypes["joy_extra"]:inject_card(v)
-            end
         end
+        JoyousSpring.made_objectpool = true
     end
 end
