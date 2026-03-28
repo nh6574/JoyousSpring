b1ba2dca01a9541a1fd3a5fa480f1ac4a334e400
N <venturinosebastian@gmail.com>
2026-03-27 23:22:30 -0300

    oops

--- src/jokers/02LiveTwin.lua
+++ src/jokers/02LiveTwin.lua
@@ -69,9 +69,6 @@ SMODS.Joker({
             text_config = { colour = G.C.CHIPS },
         }
     end,
-    calc_dollar_bonus = function(self, card)
-        return 10, { no_eval_row = true }
-    end
 })
 
 -- Live☆Twin Ki-sikil
