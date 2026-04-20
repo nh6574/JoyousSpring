3ea042f6027ab9a100afbb89c35ea16134f1ce1c
sunning838 <xxx54276343@gmail.com>
2026-04-20 21:17:50 +0900

    korean localization

---
+++ localization/ko/archetypes/26Mimighoul.lua
@@ -0,0 +1,368 @@
+return {
+    descriptions = {
+        Joker = {
+            j_joy_mimi_archfiend = {
+                name = "{C:joy_effect}미미구르 데몬",
+                text = {
+                    {
+                        "소유한 {C:joy_normal}\"미미구르\"{} 카드 또는",
+                        "뒷면 표시 {C:attention}조커{} 하나당 {C:mult}+#1#{} 배수",
+                        "{C:inactive}(현재 {C:mult}+#2#{}{C:inactive} 배수)"
+                    },
+                    {
+                        "라운드 종료 시 {C:attention}뒤집힙니다{} {C:inactive}(뒷면 표시일 경우에도 적용)",
+                    },
+                    {
+                        "{C:joy_effect}리버스{}: 자신을 파괴하고,",
+                        "{C:joy_trap}상대{} 구역에 {C:blue}일반{} {C:joy_normal}메인 덱{} {C:joy_normal}\"미미구르\"{} {C:attention}#3#{}장,",
+                        "그리고 {C:joy_effect}\"미미구르 데몬\"{}을 생성합니다",
+                        "{C:inactive}(자리가 있어야 합니다)",
+                    },
+                }
+            },
+            j_joy_mimi_armor = {
+                name = "{C:joy_effect}미미구르 아머",
+                text = {
+                    {
+                        "소유한 {C:joy_normal}\"미미구르\"{} 카드 또는",
+                        "뒷면 표시 {C:attention}조커{} 하나당 {C:chips}+#1#{} 칩",
+                        "{C:inactive}(현재 {C:chips}+#2#{}{C:inactive} 칩)"
+                    },
+                    {
+                        "라운드 종료 시 {C:attention}뒤집힙니다{} {C:inactive}(뒷면 표시일 경우에도 적용)",
+                    },
+                    {
+                        "{C:joy_effect}리버스{}: 자신을 파괴하고,",
+                        "{C:joy_trap}상대{} 구역에 {C:green}특별{} {C:joy_normal}메인 덱{} {C:joy_normal}\"미미구르\"{} {C:attention}#3#{}장,",
+                        "그리고 {C:joy_effect}\"미미구르 아머\"{}를 생성합니다",
+                        "{C:inactive}(자리가 있어야 합니다)",
+                    },
+                }
+            },
+            j_joy_mimi_cerberus = {
+                name = "{C:joy_effect}미미구르 케르베로스",
+                text = {
+                    {
+                        "{C:spell}필드 마법{}이 {X:mult,C:white}X#1#{} 배수를 줍니다"
+                    },
+                    {
+                        "라운드 종료 시 {C:attention}뒤집힙니다{} {C:inactive}(뒷면 표시일 경우에도 적용)",
+                    },
+                    {
+                        "{C:joy_effect}리버스{}: 자신을 파괴하고,",
+                        "{C:joy_trap}상대{} 구역에 {C:green}특별{} {C:joy_normal}메인 덱{} {C:joy_normal}\"미미구르\"{} {C:attention}#2#{}장,",
+                        "그리고 {C:joy_effect}\"미미구르 케르베로스\"{}를 생성합니다",
+                        "{C:inactive}(자리가 있어야 합니다)",
+                    },
+                }
+            },
+            j_joy_mimi_dragon = {
+                name = "{C:joy_effect}미미구르 드래곤",
+                text = {
+                    {
+                        "이번 런에서 {C:joy_effect}\"미미구르 드래곤\"{}에 의해",
+                        "파괴된 카드 하나당 {C:mult}+#1#{} 배수",
+                        "{C:inactive}(현재 {C:mult}+#2#{}{C:inactive} 배수)"
+                    },
+                    {
+                        "라운드 종료 시 {C:attention}뒤집힙니다{} {C:inactive}(뒷면 표시일 경우에도 적용)",
+                    },
+                    {
+                        "{C:joy_effect}리버스{}: 자신을 파괴하고,",
+                        "{C:joy_trap}상대{} 구역에 {C:attention}블라인드{} 카드 {C:attention}#3#{}장과",
+                        "{C:joy_effect}\"미미구르 드래곤\"{}을 생성합니다",
+                    },
+                }
+            },
+            j_joy_mimi_fairy = {
+                name = "{C:joy_effect}미미구르 페어리",
+                text = {
+                    {
+                        "{C:joy_trap}상대{} 구역의 카드 하나당 {C:mult}+#1#{} 배수",
+                        "{C:inactive}(현재 {C:mult}+#2#{}{C:inactive} 배수)"
+                    },
+                    {
+                        "라운드 종료 시 {C:attention}뒤집힙니다{} {C:inactive}(뒷면 표시일 경우에도 적용)",
+                    },
+                    {
+                        "{C:joy_effect}리버스{}: 자신을 파괴하고,",
+                        "{C:joy_trap}상대{} 구역에 {C:blue}일반{} {C:joy_normal}메인 덱{} {C:joy_normal}\"미미구르\"{} {C:attention}#3#{}장,",
+                        "그리고 {C:joy_effect}\"미미구르 페어리\"{}를 생성합니다",
+                        "{C:inactive}(자리가 있어야 합니다)",
+                    },
+                }
+            },
+            j_joy_mimi_flower = {
+                name = "{C:joy_effect}미미구르 플라워",
+                text = {
+                    {
+                        "{C:joy_trap}상대{} 구역의 슬롯을 2배로 늘립니다",
+                        "{C:inactive}(뒷면 표시일 경우에도 적용)"
+                    },
+                    {
+                        "라운드 종료 시 {C:attention}뒤집힙니다{} {C:inactive}(뒷면 표시일 경우에도 적용)",
+                    },
+                    {
+                        "{C:joy_effect}리버스{}: 자신을 파괴하고,",
+                        "{C:joy_trap}상대{} 구역에 {C:joy_effect}\"미미구르 플라워\"{}를 생성합니다",
+                    },
+                }
+            },
+            j_joy_mimi_slime = {
+                name = "{C:joy_effect}미미구르 슬라임",
+                text = {
+                    {
+                        "{C:attention}블라인드{} 선택 시 이 구역에 있는",
+                        "모든 {C:joy_effect}리버스{} {C:joy_normal}\"미미구르\"{}를 앞면으로 뒤집습니다",
+                        "{C:inactive}(이 카드가 뒷면 표시일 경우에도 적용)"
+                    },
+                    {
+                        "라운드 종료 시 {C:attention}뒤집힙니다{} {C:inactive}(뒷면 표시일 경우에도 적용)",
+                    },
+                    {
+                        "{C:joy_effect}리버스{}: 자신을 파괴하고,",
+                        "{C:joy_trap}상대{} 구역에 {C:blue}일반{} 또는 {C:green}특별{}",
+                        "{C:joy_normal}메인 덱{} {C:joy_normal}\"미미구르\"{} {C:attention}#1#{}장,",
+                        "그리고 {C:joy_effect}\"미미구르 슬라임\"{}을 생성합니다",
+                        "{C:inactive}(자리가 있어야 합니다)",
+                    },
+                }
+            },
+            j_joy_mimi_master = {
+                name = "{C:joy_effect}미미구르 마스터",
+                text = {
+                    {
+                        "{C:joy_trap}상대{} 구역에 있는 카드 하나당",
+                        "{X:mult,C:white}X#1#{} 배수를 줍니다"
+                    },
+                    {
+                        "{C:joy_trap}상대{} 구역의 슬롯을 2배로 늘립니다",
+                        "{C:inactive}(뒷면 표시일 경우에도 적용)"
+                    },
+                    {
+                        "라운드당 한 번, {C:attention}조커{} 1장을 {C:attention}뒤집을{} 수 있습니다",
+                    },
+                    {
+                        "라운드당 한 번, {C:joy_trap}상대{} 구역에 있는",
+                        "카드 1장을 {C:attention}뒤집을{} 수 있습니다"
+                    },
+                }
+            },
+            j_joy_mimi_giant = {
+                name = "{C:joy_xyz}자이언트 미미구르",
+                text = {
+                    {
+                        "{C:joy_trap}상대{} 구역에서 카드가 앞면으로 {C:attention}뒤집힐{} 때",
+                        "소재를 {C:attention}#1#{}개 충전합니다"
+                    },
+                    {
+                        "{C:joy_xyz}엑시즈{} {C:attention}#2#{}: {C:joy_trap}상대{} 구역의 {C:joy_normal}\"미미구르\"{} 수만큼",
+                        "해당 구역에 {C:attention}블라인드{} 카드를 생성하고, 이번 라운드 동안",
+                        "해당 구역의 {C:joy_normal}\"미미구르\"{}가 아닌 카드 수만큼 {X:mult,C:white}X#3#{} 배수를 줍니다",
+                        "{C:inactive}(현재 {X:mult,C:white}X#4#{}{C:inactive} 배수)"
+                    },
+                },
+                joy_summon_conditions = {
+                    "{C:joy_effect}리버스{} {C:attention}조커{} {C:attention}2{}장",
+                    "{s:0.9,C:inactive}({s:0.9,C:joy_xyz}엑시즈{s:0.9,C:inactive}, {s:0.9,C:joy_link}링크{} {s:0.9,C:inactive}또는 {s:0.9,C:joy_token}토큰{} {s:0.9,C:inactive}제외)",
+                }
+            },
+            j_joy_mimi_throne = {
+                name = "{C:joy_xyz}미미구르 스론",
+                text = {
+                    {
+                        "{C:joy_xyz}엑시즈{} {C:attention}#1#{}: {C:dark_edition}네거티브{} {C:joy_effect}\"미미구르 마스터\"{} {C:attention}#2#{}장 생성"
+                    },
+                    {
+                        "{C:attention}제외{}되었을 때 {C:joy_trap}상대{}의",
+                        "슬롯을 영구적으로 {C:attention}#3#{}칸 늘립니다"
+                    }
+                },
+                joy_summon_conditions = {
+                    "{C:joy_effect}리버스{} {C:joy_normal}\"미미구르\"{} {C:attention}조커{} {C:attention}2{}장",
+                    "{s:0.9,C:inactive}({s:0.9,C:joy_xyz}엑시즈{s:0.9,C:inactive}, {s:0.9,C:joy_link}링크{} {s:0.9,C:inactive}또는 {s:0.9,C:joy_token}토큰{} {s:0.9,C:inactive}제외)",
+                }
+            },
+            j_joy_mimi_dungeon = {
+                name = "{C:joy_spell}미미구르 던전",
+                text = {
+                    {
+                        "라운드당 한 번, 모든 {C:attention}조커{}가 앞면 표시라면",
+                        "모든 {C:attention}조커{}를 뒷면으로 {C:attention}뒤집을{} 수 있습니다"
+                    },
+                    {
+                        "라운드당 한 번, 모든 {C:attention}조커{}가 뒷면 표시라면",
+                        "모든 {C:attention}조커{}를 앞면으로 {C:attention}뒤집을{} 수 있습니다"
+                    },
+                    {
+                        "{C:joy_normal}\"미미구르\"{}가 {C:joy_trap}상대{} 구역에",
+                        "생성하는 카드가 {C:dark_edition}네거티브{}를 가집니다"
+                    }
+                }
+            },
+        },
+        joy_Opponent = {
+            opp_joy_mimi_archfiend = {
+                name = "{C:joy_effect}미미구르 데몬",
+                text = {
+                    {
+                        "소유한 {C:joy_normal}\"미미구르\"{} 카드 또는",
+                        "뒷면 표시 {C:attention}조커{} 하나당 {C:mult}+#1#{} 배수",
+                        "{C:inactive}(현재 {C:mult}+#2#{}{C:inactive} 배수)"
+                    },
+                    {
+                        "{C:attention}블라인드{} 선택 시 {C:joy_normal}\"미미구르\"{}나",
+                        "뒷면 표시 {C:attention}조커{}가 없다면 자신을 파괴합니다"
+                    },
+                    {
+                        "라운드 종료 시 {C:attention}뒤집힙니다{} {C:inactive}(뒷면 표시일 경우에도 적용)",
+                    },
+                    {
+                        "{C:joy_effect}리버스{}: 자신을 파괴하고, 이번 라운드 동안 손패 크기 {C:attention}+#3#{},",
+                        "그리고 {C:attention}조커{} 구역에 {C:joy_effect}\"미미구르 데몬\"{}을 생성합니다",
+                        "{C:inactive}(자리가 있어야 합니다)"
+                    },
+                }
+            },
+            opp_joy_mimi_armor = {
+                name = "{C:joy_effect}미미구르 아머",
+                text = {
+                    {
+                        "소유한 {C:joy_normal}\"미미구르\"{} 카드 또는",
+                        "뒷면 표시 {C:attention}조커{} 하나당 {C:chips}+#1#{} 칩",
+                        "{C:inactive}(현재 {C:chips}+#2#{}{C:inactive} 칩)"
+                    },
+                    {
+                        "{C:attention}블라인드{} 선택 시 {C:joy_normal}\"미미구르\"{}나",
+                        "뒷면 표시 {C:attention}조커{}가 없다면 자신을 파괴합니다"
+                    },
+                    {
+                        "라운드 종료 시 {C:attention}뒤집힙니다{} {C:inactive}(뒷면 표시일 경우에도 적용)",
+                    },
+                    {
+                        "{C:joy_effect}리버스{}: 자신을 파괴하고,",
+                        "모든 {C:joy_normal}\"미미구르\"{}와 뒷면 표시 {C:attention}조커{}들을 다음 {C:attention}블라인드{}를 선택할 때까지",
+                        "라운드 종료 시 {C:attention}제외{}시키며 {C:attention}조커{} 구역에 {C:joy_effect}\"미미구르 아머\"{}를 생성합니다",
+                        "{C:inactive}(자리가 있어야 합니다)"
+                    },
+                }
+            },
+            opp_joy_mimi_cerberus = {
+                name = "{C:joy_effect}미미구르 케르베로스",
+                text = {
+                    {
+                        "{C:spell}필드 마법{}이 {X:mult,C:white}X#1#{} 배수를 줍니다"
+                    },
+                    {
+                        "{C:attention}블라인드{} 선택 시 {C:joy_normal}\"미미구르\"{}나",
+                        "뒷면 표시 {C:attention}조커{}가 없다면 자신을 파괴합니다"
+                    },
+                    {
+                        "라운드 종료 시 {C:attention}뒤집힙니다{} {C:inactive}(뒷면 표시일 경우에도 적용)",
+                    },
+                    {
+                        "{C:joy_effect}리버스{}: 자신을 파괴하고,",
+                        "{C:joy_spell}필드 마법{}을 생성하며",
+                        "{C:attention}조커{} 구역에 {C:joy_effect}\"미미구르 케르베로스\"{}를 생성합니다",
+                        "{C:inactive}(자리가 있어야 합니다)"
+                    },
+                }
+            },
+            opp_joy_mimi_dragon = {
+                name = "{C:joy_effect}미미구르 드래곤",
+                text = {
+                    {
+                        "이번 런에서 {C:joy_effect}\"미미구르 드래곤\"{}에 의해",
+                        "파괴된 카드 하나당 {C:mult}+#1#{} 배수",
+                        "{C:inactive}(현재 {C:mult}+#2#{}{C:inactive} 배수)"
+                    },
+                    {
+                        "{C:attention}블라인드{} 선택 시 {C:joy_normal}\"미미구르\"{}나",
+                        "뒷면 표시 {C:attention}조커{}가 없다면 자신을 파괴합니다"
+                    },
+                    {
+                        "라운드 종료 시 {C:attention}뒤집힙니다{} {C:inactive}(뒷면 표시일 경우에도 적용)",
+                    },
+                    {
+                        "{C:joy_effect}리버스{}: 자신을 파괴하고,",
+                        "이 구역에 있는 {C:joy_normal}\"미미구르\"{}가 아닌 무작위 카드 1장을 파괴한 뒤",
+                        "{C:attention}조커{} 구역에 {C:joy_effect}\"미미구르 드래곤\"{}을 생성합니다",
+                        "{C:inactive}(자리가 있어야 합니다)"
+                    },
+                }
+            },
+            opp_joy_mimi_fairy = {
+                name = "{C:joy_effect}미미구르 페어리",
+                text = {
+                    {
+                        "이 구역의 카드 하나당 {C:mult}+#1#{} 배수",
+                        "{C:inactive}(현재 {C:mult}+#2#{}{C:inactive} 배수)"
+                    },
+                    {
+                        "{C:attention}블라인드{} 선택 시 {C:joy_normal}\"미미구르\"{}나",
+                        "뒷면 표시 {C:attention}조커{}가 없다면 자신을 파괴합니다"
+                    },
+                    {
+                        "라운드 종료 시 {C:attention}뒤집힙니다{} {C:inactive}(뒷면 표시일 경우에도 적용)",
+                    },
+                    {
+                        "{C:joy_effect}리버스{}: 자신을 파괴하고,",
+                        "다음 출현 가능한 {C:attention}보스 블라인드{}를 리롤(변경)하며",
+                        "{C:attention}조커{} 구역에 {C:joy_effect}\"미미구르 페어리\"{}를 생성합니다",
+                        "{C:inactive}(자리가 있어야 합니다)"
+                    },
+                }
+            },
+            opp_joy_mimi_flower = {
+                name = "{C:joy_effect}미미구르 플라워",
+                text = {
+                    {
+                        "이 구역의 슬롯을 2배로 늘립니다",
+                        "{C:inactive}(뒷면 표시일 경우에도 적용)"
+                    },
+                    {
+                        "{C:attention}블라인드{} 선택 시 {C:joy_normal}\"미미구르\"{}나",
+                        "뒷면 표시 {C:attention}조커{}가 없다면 자신을 파괴합니다"
+                    },
+                    {
+                        "라운드 종료 시 {C:attention}뒤집힙니다{} {C:inactive}(뒷면 표시일 경우에도 적용)",
+                    },
+                    {
+                        "{C:joy_effect}리버스{}: 자신을 파괴하고,",
+                        "{C:red}희귀{} {C:joy_normal}\"미미구르\"{}를 생성한 뒤",
+                        "{C:attention}조커{} 구역에 {C:joy_effect}\"미미구르 플라워\"{}를 생성합니다",
+                        "{C:inactive}(자리가 있어야 합니다)"
+                    },
+                }
+            },
+            opp_joy_mimi_slime = {
+                name = "{C:joy_effect}미미구르 슬라임",
+                text = {
+                    {
+                        "{C:attention}블라인드{} 선택 시 이 구역에 있는",
+                        "모든 {C:joy_effect}리버스{} {C:joy_normal}\"미미구르\"{}를 앞면으로 뒤집습니다",
+                        "{C:inactive}(이 카드가 뒷면 표시일 경우에도 적용)"
+                    },
+                    {
+                        "{C:attention}블라인드{} 선택 시 {C:joy_normal}\"미미구르\"{}나",
+                        "뒷면 표시 {C:attention}조커{}가 없다면 자신을 파괴합니다"
+                    },
+                    {
+                        "라운드 종료 시 {C:attention}뒤집힙니다{} {C:inactive}(뒷면 표시일 경우에도 적용)",
+                    },
+                    {
+                        "{C:joy_effect}리버스{}: 자신을 파괴하고,",
+                        "{C:joy_spell}엑스트라 덱{}에 {C:joy_normal}\"미미구르\"{}를 추가하며",
+                        "{C:attention}조커{} 구역에 {C:joy_effect}\"미미구르 슬라임\"{}을 생성합니다",
+                        "{C:inactive}(자리가 있어야 합니다)"
+                    },
+                }
+            },
+        }
+    },
+    misc = {
+        dictionary = {
+            k_joy_archetype_mimi = "미미구르",
+        }
+    }
+}
---
+++ localization/ko/archetypes/32CenturIon.lua
@@ -0,0 +1,185 @@
+return {
+    descriptions = {
+        Joker = {
+            j_joy_centur_primera = {
+                name = "{C:joy_effect}센츄리온 프리메라",
+                text = {
+                    {
+                        "{C:mult}+#1#{} 배수"
+                    },
+                    {
+                        "이 카드가 {C:joy_link}사이드 덱{}에 있다면,",
+                        "{C:joy_synchro}싱크로{} 몬스터는 디버프되거나 뒷면으로 {C:attention}뒤집히지{} 않습니다"
+                    },
+                    {
+                        "{C:joy_synchro}싱크로{} 소환의 {C:attention}소재{}로 사용될 시,",
+                        "{C:joy_link}사이드 덱{}에 {C:attention}소멸성{} {C:joy_effect}\"센츄리온 프리메라\"{}를 {C:attention}1{}장 생성하고",
+                        "상점에 {C:joy_spell}엑스트라 덱{} {C:joy_normal}\"센츄리온\"{} {C:attention}1{}장을 추가합니다"
+                    }
+                }
+            },
+            j_joy_centur_atrii = {
+                name = "{C:joy_effect}센츄리온 아토리",
+                text = {
+                    {
+                        "{C:chips}+#1#{} 칩"
+                    },
+                    {
+                        "{C:joy_synchro}싱크로{} 소환 시 어떠한 {C:attention}소재{}로도 취급될 수 있습니다",
+                    },
+                    {
+                        "{C:joy_synchro}싱크로{} 소환의 {C:attention}소재{}로 사용될 시,",
+                        "{C:joy_link}사이드 덱{}에 {C:attention}소멸성{} {C:joy_effect}\"센츄리온 아토리\"{}를 {C:attention}1{}장 생성하고",
+                        "이번 라운드 동안 손패 크기를 {C:attention}+#2#{}장 늘립니다"
+                    }
+                }
+            },
+            j_joy_centur_chimerea = {
+                name = "{C:joy_effect}센츄리온 키메라",
+                text = {
+                    {
+                        "핸드 플레이 시 {C:money}+$#1#{}"
+                    },
+                    {
+                        "이 카드가 {C:joy_link}사이드 덱{}에 있다면,",
+                        "상점에서 {C:joy_synchro}싱크로{} 몬스터의 가격이 무료가 됩니다"
+                    },
+                    {
+                        "{C:joy_synchro}싱크로{} 소환의 {C:attention}소재{}로 사용될 시,",
+                        "{C:joy_link}사이드 덱{}에 {C:attention}소멸성{} {C:joy_effect}\"센츄리온 키메라\"{}를 {C:attention}1{}장 생성하고",
+                        "상점에 {C:joy_normal}메인 덱 \"센츄리온\"{} {C:attention}1{}장을 추가합니다"
+                    }
+                }
+            },
+            j_joy_centur_trudea = {
+                name = "{C:joy_effect}센츄리온 트루디아",
+                text = {
+                    {
+                        "{X:mult,C:white}X#1#{} 배수"
+                    },
+                    {
+                        "이 카드가 {C:joy_link}사이드 덱{}에 있다면,",
+                        "{C:joy_normal}\"센츄리온\"{} 몬스터는 뒷면으로 {C:attention}뒤집히지{} 않습니다"
+                    },
+                    {
+                        "{C:joy_synchro}싱크로{} 소환의 {C:attention}소재{}로 사용될 시,",
+                        "{C:joy_link}사이드 덱{}에 {C:attention}소멸성{} {C:joy_effect}\"센츄리온 트루디아\"{}를 {C:attention}1{}장 생성하고",
+                        "상점에 {C:joy_spell}\"스탠드업 센츄리온!\"{} {C:attention}1{}장을 추가합니다"
+                    }
+                }
+            },
+            j_joy_centur_gargoyle = {
+                name = "{C:joy_effect}센츄리온 가고일 II",
+                text = {
+                    {
+                        "{X:chips,C:white}X#1#{} 칩"
+                    },
+                    {
+                        "이 카드가 {C:joy_link}사이드 덱{}에 있다면,",
+                        "{C:attention}소멸성{} 카드들이 {X:mult,C:white}X#2#{} 배수를 제공합니다"
+                    },
+                    {
+                        "{C:joy_synchro}싱크로{} 소환의 {C:attention}소재{}로 사용될 시,",
+                        "{C:joy_link}사이드 덱{}에 {C:attention}소멸성{} {C:joy_effect}\"센츄리온 가고일 II\"{}를 {C:attention}1{}장 생성하고",
+                        "{C:joy_normal}\"센츄리온\"{} 몬스터 {C:attention}1{}장을 {C:attention}소생{}시킵니다"
+                    }
+                }
+            },
+            j_joy_centur_emeth = {
+                name = "{C:joy_effect}센츄리온 에메스 VI",
+                text = {
+                    {
+                        "득점한 핸드에서",
+                        "가장 왼쪽에 있는 카드를 두 번 다시 발동합니다"
+                    },
+                    {
+                        "이 카드가 {C:joy_link}사이드 덱{}에 있다면, {C:attention}묘지{}에 있는",
+                        "{C:joy_normal}\"센츄리온\"{} 1장당 {C:joy_synchro}싱크로{} 몬스터가 {C:mult}+#1#{} 배수를 줍니다"
+                    },
+                    {
+                        "{C:joy_synchro}싱크로{} 소환의 {C:attention}소재{}로 사용될 시,",
+                        "{C:joy_link}사이드 덱{}에 {C:attention}소멸성{} {C:joy_effect}\"센츄리온 에메스 VI\"{}를 {C:attention}1{}장 생성하고",
+                        "{C:joy_normal}\"센츄리온\"{} 몬스터 {C:attention}2{}장을 {C:attention}묘지{}로 보냅니다"
+                    }
+                }
+            },
+            j_joy_centur_primus = {
+                name = "{C:joy_synchro}센츄리온 프리메라 프리무스",
+                text = {
+                    {
+                        "이번 런에서 {C:attention}소환{}된 {C:joy_synchro}싱크로{} 몬스터",
+                        "1장당 {X:mult,C:white}X#1#{} 배수를 획득합니다",
+                        "{C:inactive}(현재 {X:mult,C:white}X#2#{}{C:inactive} 배수)"
+                    },
+                    {
+                        "이 카드와 다른 {C:joy_synchro}싱크로{} 몬스터 1장을 {C:attention}릴리스{}하여",
+                        "{C:joy_spell}엑스트라 덱{}에 {C:attention}소멸성{} {C:joy_normal}\"센츄리온\"{} {C:attention}2{}장을",
+                        "추가할 수 있습니다"
+                    }
+                },
+                joy_summon_conditions = {
+                    "{C:joy_synchro}튜너{} {C:attention}1{}장 +",
+                    "튜너 이외의 몬스터 {C:attention}1{}장",
+                    "{s:0.9,C:inactive}({s:0.9,C:joy_xyz}엑시즈{s:0.9,C:inactive} 또는 {s:0.9,C:joy_link}링크{s:0.9,C:inactive} 제외)",
+                }
+            },
+            j_joy_centur_auxila = {
+                name = "{C:joy_synchro}센츄리온 아크실라",
+                text = {
+                    {
+                        "{C:joy_link}사이드 덱{}에 있는 카드",
+                        "1장당 {X:mult,C:white}X#1#{} 배수를 획득합니다",
+                        "{C:inactive}(현재 {X:mult,C:white}X#2#{}{C:inactive} 배수)"
+                    },
+                    {
+                        "{C:joy_synchro}싱크로{} 소환의 {C:attention}소재{}로 사용될 시,",
+                        "{C:joy_link}사이드 덱{}에 {C:attention}소멸성{} {C:joy_normal}메인 덱 \"센츄리온\"{} {C:attention}1{}장을 생성하고",
+                        "{C:joy_spell}엑스트라 덱{}에 {C:joy_normal}\"센츄리온\"{} {C:attention}1{}장을 추가합니다"
+                    }
+                },
+                joy_summon_conditions = {
+                    "{C:joy_synchro}튜너{} {C:attention}1{}장 +",
+                    "튜너 이외의 몬스터 {C:attention}1{}장",
+                    "{s:0.9,C:inactive}({s:0.9,C:joy_xyz}엑시즈{s:0.9,C:inactive} 또는 {s:0.9,C:joy_link}링크{s:0.9,C:inactive} 제외)",
+                }
+            },
+            j_joy_centur_legatia = {
+                name = "{C:joy_synchro}센츄리온 레가티아",
+                text = {
+                    {
+                        "{C:joy_link}사이드 덱{}에 있는 {C:joy_normal}\"센츄리온\"{} 카드들이",
+                        "마치 필드에 보유 중인 것처럼 점수를 계산합니다",
+                        "{C:inactive,s:0.85}(단, 엑스트라 덱 조커들은 정규 소환된 상태여야 합니다){}"
+                    },
+                    {
+                        "라운드 종료 시 {C:joy_spell}엑스트라 덱{}으로 돌아갑니다.",
+                        "돌아갔을 경우, 이번 라운드 동안",
+                        "슬롯이 꽉 차 있어도 {C:joy_link}사이드 덱{} 스킵을 허용합니다"
+                    }
+                },
+                joy_summon_conditions = {
+                    "{C:joy_synchro}싱크로{} {C:joy_synchro}튜너{} {C:attention}1{}장 +",
+                    "튜너 이외의 {C:joy_synchro}싱크로{} 몬스터 {C:attention}1{}장",
+                }
+            },
+            j_joy_centur_standup = {
+                name = "{C:joy_spell}스탠드업 센츄리온!",
+                text = {
+                    {
+                        "라운드당 한 번, 소환에 사용된 모든 카드를",
+                        "{C:joy_link}사이드 덱{}에 {C:attention}소멸성{} 상태로 생성합니다"
+                    },
+                    {
+                        "카드가 {C:attention}소멸(기간 만료){}할 경우,",
+                        "{C:joy_link}사이드 덱{}에 {C:attention}소멸성{} {C:joy_normal}\"센츄리온\"{} {C:attention}1{}장을 생성합니다"
+                    }
+                }
+            },
+        }
+    },
+    misc = {
+        dictionary = {
+            k_joy_archetype_centur = "센츄리온",
+        }
+    }
+}
